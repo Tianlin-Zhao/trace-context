2c8006e61715493918f918504bc00da804e31958
Tianlin Zhao <tianlinzhao@google.com>
2020-08-16 20:53:16 -0400

    print

--- test/test.py
+++ test/test.py
@@ -52,6 +52,7 @@ class TestBase(unittest.TestCase):
 			for idx in range(count):
 				arguments['arguments'].append({'url': scope.url(str(idx)), 'arguments': []})
 			response = scope.send_request(arguments = arguments)
+			print(response)
 			verbose = ['', '']
 			verbose.append('Harness trying to send the following request to your service {0}'.format(arguments['url']))
 			verbose.append('')
